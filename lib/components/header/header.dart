470023f3cdc9d29abe58a6b221106fdb2579d012
trancore <ki.program0915@gmail.com>
2025-05-06 20:59:05 +0900

    feature: add header component

---
+++ lib/components/header/header.dart
@@ -0,0 +1,22 @@
+﻿import 'package:flutter/material.dart';
+import 'package:onaka_diary/constants/app.dart';
+
+class Header extends StatefulWidget implements PreferredSizeWidget {
+  const Header({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 245, 237),
+        title: const Text(AppConstants.appName,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)));
+  }
+}
